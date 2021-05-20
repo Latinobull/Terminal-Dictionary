f7442ebe7c44898f2d7eed0974b849210ab11b8c
Donnahue George <Donnahuegjr@gmail.com>
2021-05-20 15:06:38 -0400

    if user types an incorrect word, provides a suggestion for the user if they wanted that word instead

--- index.py
+++ index.py
@@ -12,6 +12,19 @@ def getWord(word):
         return data[word.title()]
     elif word.upper() in data:
         return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input(
+            "Did you mean %s instead? Enter Y if yes, or N if no:"
+            % get_close_matches(word, data.keys())[0]
+        )
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "Sorry we did not understand your entry"
+    else:
+        return word + " does not exist. Please check your spelling."
 
 
 word = input("Which word do you want to search? ")
