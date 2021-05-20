0a07cfc3025d5d9b2f3a861f6dbebb42bb2cb9f6
Donnahue George <Donnahuegjr@gmail.com>
2021-05-20 15:05:05 -0400

    allows places that require uppercase words to be read in the data file

--- index.py
+++ index.py
@@ -8,6 +8,10 @@ def getWord(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
 
 
 word = input("Which word do you want to search? ")
